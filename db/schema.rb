40985927fb7b6e63979d8a4903db092f9aba99e6
Jason <jzhao.690@gmail.com>
2015-07-10 15:14:34 -0700

    fixed event migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150710221118) do
+ActiveRecord::Schema.define(version: 20150710221414) do
+
+  create_table "events", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "date"
+    t.string   "details"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
