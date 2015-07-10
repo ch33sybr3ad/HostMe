daf5ce788dabbc02c9cbf7151c7c846cf09f0cce
Jason <jzhao.690@gmail.com>
2015-07-10 15:24:59 -0700

    create user and event models

--- db/migrate/20150710222420_create_users.rb
+++ db/migrate/20150710222420_create_users.rb
@@ -2,8 +2,8 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :name
-      t.string :email
-      t.string :password
+      t.integer :email
+      t.integer :password
 
       t.timestamps null: false
     end
