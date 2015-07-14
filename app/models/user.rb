69d1212d09b04e894fabc1b96d1105902c40adc0
Jason <jzhao.690@gmail.com>
2015-07-13 20:59:20 -0700

    added gravatar images:

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  include Gravtastic
+  gravtastic
+
   has_many :events
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   get 'pages/index'
-  resources :events
-  resources :users
+  resources :events, only: [:index, :show]
+  resources :users, only: [:index, :show]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
